1a741a57af8c8f792f77a67c496afcc5a408ec8d
aastha <aasthg948@gmail.com>
2026-06-30 23:18:43 +0530

    writinf js code in ejs file

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, "public")));
 
+const filesDir = path.join(__dirname, "files");
+
 app.get('/', function(req, res){
-    res.render("index.ejs");
-})
+    const files = fs.readdirSync(filesDir);
+    const tasks = files.map(file => {
+        const content = fs.readFileSync(path.join(filesDir, file), "utf-8");
+        const [title, ...detailLines] = content.split("\n");
+        return { filename: file, title, details: detailLines.join("\n") };
+    });
+    res.render("index.ejs", { tasks });
+});
+
+app.post('/create', function(req, res){
+    const { title, details } = req.body;
+    const filename = title.split(" ").join("_") + ".txt";
+    fs.writeFileSync(path.join(filesDir, filename), title + "\n" + details);
+    res.redirect("/");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
